f3a121b7e82ad05304a4e5fea0b9ce99b8345e5d
Aprendez14 <founoir@hotmail.com>
2015-08-17 13:51:15 +0100

    + hipervinculos al endpoint de usuario

--- users/serializers.py
+++ users/serializers.py
@@ -5,4 +5,4 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'name', 'golden_badges', 'silver_badges', 'bronze_badges', 'points', 'level', 'percent_in_level')#, 'last_login')
+        fields = ('url', 'id', 'name', 'golden_badges', 'silver_badges', 'bronze_badges', 'points', 'level', 'percent_in_level')#, 'last_login')
